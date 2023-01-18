3feede22cd739fea69ba8ce3df42d9ca3f14c05d
Olawoyin Temidayo <t.olawoyin@outlook.com>
2023-01-18 06:31:30 +0100

    added the traverseMethods to graph

--- buildgraph.mjs
+++ buildgraph.mjs
@@ -25,12 +25,45 @@ class BuildGraph {
         }
         this.graph.delete(node);
     }
+    // Traversing graph with BFS
+    traverseBFS(from) {
+        let queue = [], visited = {};
+        queue.push(from);
+        while(queue.length) {
+            let current = queue.shift();
+            visited[current] = true;
+            for(let node of this.graph.get(current).keys()) {
+                if(!visited[node]) queue.push(node);
+            }
+        }
+    }
+    // Traversing graph with DFS
+
+    //  The traverseDFS works by recursively visiting all nodes
+    traverseDFS(from) {
+        let visited = {};
+        return this._traverseDFS(from, visited)
+    }
+    _traverseDFS(from, visited) {
+        visited[from] = true;
+        console.log(visited)
+        for(let node of this.graph.get(from).keys()) {
+            if(!visited[node]) this._traverseDFS(node, visited)
+        }
+    }
 }
 
 const graph = new BuildGraph;
 graph.addNode("A")
 graph.addNode("B")
+graph.addNode("C")
+graph.addNode("D")
 graph.addEdge("A", "B", 3)
+graph.addEdge("A", "C", 3)
+graph.addEdge("B", "C", 3)
+graph.addEdge("B", "D", 3)
+graph.addEdge("D", "C", 3)
 // graph.removeEdge("A", "B")
-graph.removeNode("A")
-console.log(graph)
+graph.traverseBFS("A")
+graph.traverseDFS("A", "D")
+// console.log(graph)
