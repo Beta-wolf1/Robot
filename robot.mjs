9b047f83ee1df3e673e1cba3e590802eba0d2cb7
Olawoyin Temidayo <t.olawoyin@outlook.com>
2023-01-18 07:48:01 +0100

    working on it

---
+++ robot.mjs
@@ -0,0 +1,21 @@
+import { graph } from "./buildgraph.mjs";
+
+const runRobot = (state, robot, memory) => {
+    for(let turns = 0;; turns++) {
+        if(state.parcels.length == 0) {
+            console.log("Done");
+            break;
+        }
+        let action = robot(state, memory); // what the robot returns matters
+    }
+}
+
+// Omo, it's like head yaff dry. Anytime I leave javascript to learn something, it takes hours of coding to reboot the head back. O ma se o....
+// programming is very volatile. Will I have the time to even teach it to fisayo...
+
+// i will surely need graph for this endeavor
+const random = (place) => place[Math.floor(Math.random() * place.length)];
+
+const randomRobot = (state) => {
+    return {destination: random(state.place)}
+}
